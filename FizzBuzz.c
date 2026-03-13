022d2e628857d7d096a1f9d536019f5a812579d4
sree8r8bee27-byte <sree8r8_bee27@mepcoeng.ac.in>
2026-03-13 11:22:33 +0530

    Implement FizzBuzz function in C

---
+++ FizzBuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void fizzBuzz(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+}
+
+int main() {
+    printf("FizzBuzz (1-100):\n");
+    fizzBuzz(100);
+    return 0;
+}
